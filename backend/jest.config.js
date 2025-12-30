dfbe4b9b69fe15edefc6b88d656d23ee078d590b
shruti singh <shrutisinghskr@gmail.com>
2025-12-30 21:08:13 +0530

    creating simple Jest to test

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+﻿/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+};
+
+module.exports = config;
---
+++ backend/tests/simple.test.ts
@@ -0,0 +1,5 @@
+describe('Simple test', () => {
+    test('Should pass', () => {
+        expect(1 + 1).toBe(2);
+    })
+})
